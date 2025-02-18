578bdef23a2876ff43d29e2b22193dadf2a605d2
momarzouq <smsabomarzoq@gmail.com>
2025-02-18 09:42:47 +0200

    install react-player and show video

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import VideoComponent from "./VideoPlayer";
 
 function App() {
-  return <>
-  </>;
+  return (
+    <>
+      <h1>Video Player</h1>
+      <VideoComponent />
+    </>
+  );
 }
 
 export default App;
